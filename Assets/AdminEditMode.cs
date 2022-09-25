9f4316e59f34a3cc4515c5d47c5ffb41d602556c
Marat <maratg2develop@gmail.com>
2022-09-25 21:49:24 +0400

    Edit cursor over preview

--- Assets/AdminEditMode.cs
+++ Assets/AdminEditMode.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class AdminEditMode : MonoBehaviour
 {
+    [SerializeField] private AdminViewMode _adminView;
     [SerializeField] private RectTransform _cursorTile;
+    [SerializeField] private RectTransform _imagePreview;
 
     void Update()
     {
         Vector2 windowSize = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
-        float mouseX = Input.mousePosition.x;
-        if (mouseX < 0.75f * windowSize.x)
-            _cursorTile.anchoredPosition = Input.mousePosition;
+        Vector2 absoluteMousePos = Input.mousePosition;
+        float tileSize = _imagePreview.sizeDelta.x / _adminView.HallSelected.sizex;
+        _cursorTile.sizeDelta = new Vector2(tileSize, tileSize);
+
+        float addPosX = 0, addPosY = tileSize / 4;
+        if(_adminView.HallSelected.sizez % 2 == 0)
+            addPosY = -tileSize / 4;
+        if (_adminView.HallSelected.sizex % 2 != 0)
+            addPosX = tileSize / 2;
+        
+        _imagePreview.anchoredPosition = new Vector2
+        (
+            Mathf.FloorToInt((0.4f) * (windowSize.x / tileSize)) * tileSize + addPosX,
+            Mathf.FloorToInt((0.5f) * (windowSize.y / tileSize)) * tileSize + addPosY
+        );
+        Vector2 tiledMousePos = new Vector2
+        (
+            Mathf.FloorToInt((absoluteMousePos.x / windowSize.x) * (windowSize.x / tileSize)) * tileSize + tileSize/2,
+            Mathf.FloorToInt((absoluteMousePos.y / windowSize.y) * (windowSize.y / tileSize)) * tileSize + tileSize/4
+        );
+        if (absoluteMousePos.x < 0.75f * windowSize.x)
+            _cursorTile.anchoredPosition = tiledMousePos;
         else
             _cursorTile.anchoredPosition = -windowSize;
     }
