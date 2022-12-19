c6cb2a4d091932c1e697e1063ebe1f7a7280be42
Marat <maratg2develop@gmail.com>
2022-12-19 10:55:47 +0400

    RoomsContainer

--- Assets/Scripts/RoomsContainer.cs
+++ Assets/Scripts/RoomsContainer.cs
@@ -10,25 +10,57 @@ using UnityEngine.Networking;
 
 public class RoomsContainer : MonoBehaviour
 {
-    public Dictionary<int, RoomDto> CachedRooms;
-    public List<Hall> CachedHallsInfo;
+    public Dictionary<int, RoomDto> CachedRooms { get; private set; }
+    public List<Hall> CachedHallsInfo { get; private set; }
     private HallQueriesAsync _hallQueriesAsync = new();
     private HallQueries _hallQueries = new();
     private WWWForm form;
     private readonly string baseRoute = "https://istu-museum-admin.netlify.app/api/PHP/";
 
-    /*private void OnEnable()
+    private void OnEnable()
     {
         _hallQueries.OnAllHallsGet += halls => CachedHallsInfo = halls;
-    }*/
+        _hallQueries.OnAllHallContentsGet += AddToCachedRooms;
+    }
+    
+    private void OnDisable()
+    {
+        _hallQueries.OnAllHallsGet -= halls => CachedHallsInfo = halls;
+        _hallQueries.OnAllHallContentsGet -= AddToCachedRooms;
+    }
+
+    private void AddToCachedRooms(List<HallContent> newContents)
+    {
+        if (newContents.Count > 0)
+        {
+            CachedRooms[newContents[0].hnum].Contents = newContents;
+        }
+    }
+    
     public void Awake()
     {
+        StartCoroutine(InitializeRoomsContainer());
         form = new WWWForm();
+    }
+
+    private IEnumerator InitializeRoomsContainer()
+    {
+        CachedRooms = new Dictionary<int, RoomDto>();
+        CachedHallsInfo = new List<Hall>();
+        yield return _hallQueries.GetAllHalls();
+        foreach (var hall in CachedHallsInfo)
+        {
+            var newRoomDto = new RoomDto();
+            newRoomDto.HallOptions = hall;
+            newRoomDto.Contents = new List<HallContent>();
+            CachedRooms.Add(hall.hnum, newRoomDto);
+            yield return _hallQueries.GetAllContentsByHnum(hall.hnum);
+        }
     }
 
     public async void Start()
     {
-        await RefreshHallsInfo().ConfigureAwait(false);
+        //await RefreshHallsInfo().ConfigureAwait(false);
     }
 
     public async Task RefreshHallsInfo()
