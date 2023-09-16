898a37fafb239a9f364502c24187f932b9919fe6
Marat <maratg2develop@gmail.com>
2023-09-16 13:29:22 +0400

    FPS lock, low quality settings

---
+++ Assets/Scripts/InProject/SetFPS.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SetFPS : MonoBehaviour
+{
+    [SerializeField] private int _targetFPS = 30;
+     
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = _targetFPS;
+    }
+     
+    void Update()
+    {
+        if(Application.targetFrameRate != _targetFPS)
+            Application.targetFrameRate = _targetFPS;
+    }
+}
