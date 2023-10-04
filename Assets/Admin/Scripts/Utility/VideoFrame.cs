780863c19dc2fdf788d1daf7def4189d4f343461
Marat <maratg2develop@gmail.com>
2023-10-04 21:59:06 +0400

    Scale video too, better fit

--- Assets/Admin/Scripts/Utility/VideoFrame.cs
+++ Assets/Admin/Scripts/Utility/VideoFrame.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -20,14 +21,35 @@ namespace Admin.Utility
             videoPlayer.url = videoUrl;
             videoPlayer.Pause();
             videoPlayer.SetDirectAudioVolume(0, volume);
+            StopAllCoroutines();
+            StartCoroutine(SetSize());
         }
 
          public void Interact()
         {
             if (_isPlayed) videoPlayer.Pause();
-            else videoPlayer.Play();
+            else
+            {
+                videoPlayer.Play();
+            }
             _isPlayed = !_isPlayed;
         }
+
+         private IEnumerator SetSize()
+         {
+             yield return new WaitUntil(() => videoPlayer.isPrepared);
+             
+             Vector2 initScale = new Vector2(1f, 1.7f);
+             initScale /= 1.5f;
+             float xMulti = videoPlayer.width / (float)videoPlayer.height;
+             Debug.Log(xMulti);
+             Mathf.Clamp(xMulti, 0.3f, 3.3f);
+             if (xMulti >= 1f)
+                 initScale.x *= xMulti;
+             else
+                 initScale.y /= xMulti;
+             transform.localScale = new Vector3(initScale.x, initScale.y, 1f);
+         }
 
          public bool IsOpen()
          {
